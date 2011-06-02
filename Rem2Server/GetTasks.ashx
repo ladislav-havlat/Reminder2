9d46f01071d43dbd34c11670c6102b4fc0b4d379
Ladislav Havlát <ladislav.havlat@gmail.com>
2011-06-02 16:41:24 +0200

    + Rem2Server project

---
+++ Rem2Server/GetTasks.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="GetTasks.ashx.cs" Class="Rem2Server.GetTasks" %>
---
+++ Rem2Server/GetTasks.ashx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Rem2Server
+{
+    /// <summary>
+    /// Summary description for $codebehindclassname$
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    public class GetTasks : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "application/xml";
+            XmlTextWriter respWriter = new XmlTextWriter(context.Response.Output);
+
+            respWriter.WriteStartDocument(true);
+            respWriter.WriteStartElement("reminder");
+            respWriter.WriteEndElement();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
